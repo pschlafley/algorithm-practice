9aafb09d2a774cf039cfd6080a35c87807c499f2
Peyton Schlafley <pschlafley0@gmail.com>
2021-06-20 16:39:21 -0500

    fixed solution to the title-case algorithm so that the sollution passes

--- freecodecamp-algo/title-case/index.js
+++ freecodecamp-algo/title-case/index.js
@@ -1,19 +1,33 @@
 function titleCase(str) {
-    let splitStr = str.split(' ')
+    // seperate each word by splitting str into an array 
+    let splitStr = str.split(' ');
+    // create empty string and array variables
     let aStr = "";
+    let words = [];
+    let finalStr = '';
 
+    // loop through splitStr so that we can isolate each word
     for (let i = 0; i < splitStr.length; i++) {
+        // isolate each word in splitStr array
         let word = splitStr[i];
+        // get the first letter of each word
         let bStr = word.substr(0, 1);
+        // get the remaining letters after the first letter for each word
         let cStr = word.substr(1, word.length);
 
+        // make every first letter upper case
         let bStrUp = bStr.toUpperCase();
+        // make every other letter in each word lower case
         let cStrLow = cStr.toLowerCase();
 
-        aStr += bStrUp + cStrLow + ' ';
+        // add the capitalized first letters of each word to the rest of the lower case letters
+        aStr = bStrUp + cStrLow;
+        // push aStr into the words array
+        words.push(aStr);
     }
-    console.log(aStr);
-    return aStr;
-};
+    // call the join method on the words array so that it creates spaces in between words but removes unnecessary white space at the end of the string
+    finalStr = words.join(' ');
 
-titleCase("I'm a little tea pot");
+    // return the final string with each first letter capitalized and the remaining letters lower case
+    return finalStr;
+};
