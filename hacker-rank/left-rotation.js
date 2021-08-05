45e3359e59e43b447227eea5bb54765d463df458
Peyton Schlafley <pschlafley0@gmail.com>
2021-08-04 20:51:14 -0400

    solved other algos

---
+++ hacker-rank/left-rotation.js
@@ -0,0 +1,17 @@
+function rotLeft(a, d) {
+    let tracker = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        if (tracker > d) {
+            return;
+        }
+
+        while (tracker < d) {
+            // remove the first element in the array
+            // then push that element to the end of the array to "rotate it to the left"
+            a.push(a.shift())
+            tracker++;
+        }
+    }
+    return a;
+}
