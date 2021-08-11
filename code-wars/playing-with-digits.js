da44baab4b386e21ac1c6008274c2c89f9d0bbb9
Peyton Schlafley <pschlafley0@gmail.com>
2021-08-11 18:11:58 -0400

    solved another algorithm from codewars

---
+++ code-wars/playing-with-digits.js
@@ -0,0 +1,25 @@
+function digPow(n, p) {
+    n = String(n);
+    let nArr = n.split('');
+    let sum = 0;
+    let k = 0;
+
+    nArr.reduce((acc, num, index) => {
+        let numPower = Math.pow(num, (p + index));
+        sum += numPower;
+    }, 0);
+
+    k += sum / n;
+    let stringK = k.toString();
+
+    if (stringK.includes('.')) {
+        return -1;
+    }
+
+    if (n * k === sum) {
+        return k;
+    }
+
+
+    return -1;
+}
--- freecodecamp-algo/boo-who/index.js
+++ freecodecamp-algo/boo-who/index.js
@@ -11,3 +11,4 @@ function booWho(bool) {
 }
 
 booWho(null);
+
